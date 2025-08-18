5e2ea9e55dce1a3216808d6f39967be07f5d132a
Basanta <basantaparajuli.11@gmail.com>
2025-08-18 17:54:51 +0545

    imprting all entities

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comment, Post, User])],
   controllers: [CommentsController],
-  providers: [CommentsService]
+  providers: [CommentsService],
 })
 export class CommentsModule {}
