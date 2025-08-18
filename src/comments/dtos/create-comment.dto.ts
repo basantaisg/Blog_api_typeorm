b8f3ef61a2dd200889ba5be20f930875908b6448
Basanta <basantaparajuli.11@gmail.com>
2025-08-18 17:58:20 +0545

    create-comment and upate-comment dto

---
+++ src/comments/dtos/create-comment.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  text: string;
+  @IsNumber()
+  userId: number;
+  @IsNumber()
+  postId: number;
+}
