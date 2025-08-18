df137fb780ed574585c83f3be6c7dbcf2216f000
Basanta <basantaparajuli.11@gmail.com>
2025-08-18 16:46:53 +0545

    creation of create-user.dto.ts

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(20)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(50)
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(16)
+  password: string;
+}
